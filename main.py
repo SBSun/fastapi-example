c6cbc1d142808b398899388d88f931f73d4adc1d
SBSun <byung0216@gmail.com>
2024-12-30 00:07:58 +0900

    [FEAT] api 요청에 대한 middleware 추가

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+import time
+
 from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
 from starlette.responses import JSONResponse
@@ -12,6 +15,12 @@ app.container = Container()
 app.include_router(user_routers)
 app.include_router(note_routers)
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+
 @app.get("/")
 def hello():
   return {"Hello": "FastAPI"}
@@ -22,3 +31,22 @@ async def validation_exception_handler(
     exc: RequestValidationError
 ):
   return JSONResponse(status_code=400, content=exc.errors(),)
+
+@app.middleware("http")
+async def http_middleware(request: Request, call_next):
+  # 요청 정보
+  method = request.method
+  url = str(request.url)
+  http_version = request.scope.get("http_version", "1.1")
+
+  start_time = time.time()
+  # 미들웨어가 여러 개 등록돼 있다면 다음 미들웨어로 전달하거나 엔드 포인트 함수로 요청을 전달한다.
+  response = await call_next(request)
+
+  status_code = response.status_code
+  logging.info(f"{method} {url} HTTP/{http_version} {status_code}")
+
+  process_time = time.time() - start_time
+  response.headers["X-Process-Time"] = str(process_time)
+
+  return response
